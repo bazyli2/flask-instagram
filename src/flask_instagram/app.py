08dc700621fa4c76e4e68b3ae28372a1eff754b8
Jeremi Wielewski <jeremi.wielewski@profil-software.com>
2025-03-14 13:57:36 +0100

    Integrate Flask-Login with application factory
    - Added `login.py` module to initialize `LoginManager`
    - Updated `create_app` to initialize `login_manager` with the app

--- src/flask_instagram/app.py
+++ src/flask_instagram/app.py
@@ -5,4 +5,7 @@ def create_app():
     app = Flask(__name__)
     with app.app_context():
         import flask_instagram.routes
+        from flask_instagram.login import login_manager
+
+        login_manager.init_app(app)
     return app
