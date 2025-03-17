d2ae1538efc134f6dff100c3d19970e261c0c3b2
Jeremi Wielewski <jeremi.wielewski@profil-software.com>
2025-03-17 14:14:01 +0100

    Add SignUpForm with password validation for digit and special character

--- src/flask_instagram/forms.py
+++ src/flask_instagram/forms.py
@@ -1,8 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email
+from wtforms import PasswordField, StringField
+from wtforms.validators import DataRequired, Email, Length, Regexp
 
 
 class LoginForm(FlaskForm):
     email = StringField("email", validators=[DataRequired(), Email()])
     password = StringField("password", validators=[DataRequired()])
+
+
+class SignUpForm(FlaskForm):
+    email = StringField("email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        "password",
+        validators=[
+            DataRequired(),
+            Length(8, 72),
+            Regexp(r"\d", message="Password must include at least one digit"),
+            Regexp(
+                "[$&+,:;=?@#|'<>.^*()%!-]",
+                message="Password must include at least one special character",
+            ),
+        ],
+    )
