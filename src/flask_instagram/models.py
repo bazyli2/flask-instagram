b552434855e6ca67d218e65fcfe896b5713a8d55
Jeremi Wielewski <jeremi.wielewski@profil-software.com>
2025-03-17 10:19:45 +0100

    feat: add User model and initial database migration
    - Introduced `User` model with `id`, `email`, and `password_hash` fields.
    - Integrated `UserMixin` for Flask-Login support.
    - Created initial Alembic migration to add the `user` table with a unique index on `email`.

--- src/flask_instagram/models.py
+++ src/flask_instagram/models.py
@@ -1,5 +1,14 @@
-from sqlalchemy.orm import DeclarativeBase
+from flask_login import UserMixin
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class User(Base, UserMixin):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), index=True, unique=True)
+    password_hash: Mapped[str] = mapped_column(String(256))
